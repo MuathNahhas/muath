378b168e937b72783615fb05372e8ef7faa053e3
MuathNahhas <muath.nahhas@yahoo.com>
2021-07-12 22:05:08 +0300

    test

---
+++ main.js
@@ -0,0 +1,53 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+const squre= function(number){
+
+console.log(number*number);
+};
+const sayHello = function () {
+    return "hello";
+  };
+
+  const myAge = function (age) {
+    console.log(age);
+  };
+
+  const incrementOne = function (number) {
+    return number+1;
+  };
+
+  const greet = function (name) {
+    return name +"hello";
+  };
+  const double = function (number) {
+    return number*number;
+  };
+
+  const fullName = function (firstName, lastName) {
+    return firstName+ lastName;
+  };
+
+  const average = function (a, b) {
+    return (a+b)/2;
+  };
+
+  95
+  18
+  
+ /* const calculateProfit = function (unitsSold, unitCost, unitPrice) {
+    return (unitPrice*unitsSold)-(unitsSold*unitCost);
+  };
+  const floor = function (number) {
+    return Math.round(number);
+  };
+  */
+
+  const toThePowerOf = function (base, exponent) {
+    return Math.pow(base,exponent);
+  };
+//Q10
+  const upperCase = function (string) {
+    return string.toUpperCase();
+  };
+
